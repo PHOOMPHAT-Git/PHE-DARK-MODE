9d328cb046e39e8e7214d9b81668246508f60030
Phe <phoomphat091@gmail.com>
2024-12-11 23:59:23 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date('December 11, 2024 19:30:00').getTime();
+const targetDate = new Date('December 14, 2024 19:30:00').getTime();
 
 const countdownInterval = setInterval(function() {
     const now = new Date().getTime();
